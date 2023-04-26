a89f4dfdb5b29056727b00f76b8d5c7afdf1f9f2
Uri Bar <urib@monada.co>
2023-04-26 18:37:24 +0300

    distance calc

--- dogfooding/where-to-eat/fetch.js
+++ dogfooding/where-to-eat/fetch.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const google_places_url =
   "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=1000&type=restaurant&location={location}&keyword={keyword}&key={key}";
 
-  exports.list_restaurants = async function (location, keyword, key) {
+exports.list_restaurants = async function (location, keyword, key) {
   const res = await fetch(
     google_places_url
       .replace("{location}", location)
@@ -20,11 +20,18 @@ const google_places_url =
   } else {
     let response_body = await res.json();
     let result_restaurants = [];
+    let user_location_lat = parseFloat(location.split(",")[0]);
+    let user_location_lng = parseFloat(location.split(",")[1]);
     for (let i = 0; i < response_body.results.length; i++) {
       const result = response_body.results[i];
       // Filter out restaurants whose business_status is different from "OPERATIONAL"
       if (result.business_status === "OPERATIONAL") {
-        let rest = transformResultToRestaurant(result, keyword);
+        let rest = transformResultToRestaurant(
+          result,
+          user_location_lat,
+          user_location_lng,
+          keyword
+        );
         result_restaurants.push(rest);
       }
     }
@@ -33,9 +40,19 @@ const google_places_url =
 };
 
 // TODO: move this to wing?
-function transformResultToRestaurant(result, keyword) {
+function transformResultToRestaurant(
+  result,
+  user_location_lat,
+  user_location_lng,
+  keyword
+) {
   return {
-    location: result.geometry.location,
+    distance: getDistanceFromLatLonInKm(
+      user_location_lat,
+      user_location_lng,
+      result.geometry.location.lat(),
+      result.geometry.location.lng()
+    ),
     name: result.name,
     rating: result.rating,
     user_ratings_total: result.user_ratings_total,
@@ -43,4 +60,23 @@ function transformResultToRestaurant(result, keyword) {
     open_now: result.opening_hours?.open_now,
     search_keyword: keyword,
   };
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d * 1000; // Distance in m
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
 }
